a1254305348a58a9f0296913501ec8e89cfa1d3d
jopizmed <58296616+jopizmed@users.noreply.github.com>
2020-12-19 19:25:53 +0100

    Add files via upload

--- Lab1_AJAX/includes/js/code.js
+++ Lab1_AJAX/includes/js/code.js
@@ -1,24 +1,23 @@
+// Se quitan los caracteres especiales
 String.prototype.transformaCaracteresEspeciales = function() {
 		return unescape(escape(this).
             replace(/%0A/g, '<br/>').
             replace(/%3C/g, '&lt;').
             replace(/%3E/g, '&gt;'));
 	}
-	
-	var estadosPosibles = ['No inicializado', 'Cargando', 'Cargado', 'Interactivo', 'Completado'];
-  
-	var tiempoInicial = 0;
+	var states = ['No inicializado', 'Cargando', 'Cargado', 'Interactivo', 'Completado'];
+	var initTime = 0;
   
 	window.onload = function() {
-		// Cargar en el input text la URL de la página
+		// Cargar la URL de la página en el campo Text
 		var recurso = document.getElementById('recurso');
 		recurso.value = location.href;
 
-		// Cargar el recurso solicitado cuando se pulse el botón MOSTRAR CONTENIDOS
-		document.getElementById('enviar').onclick = cargaContenido;
+		// Cargar el recurso solicitado cuando se haga 'clic' en el botón
+		document.getElementById('enviar').onclick = loadContent;
 	}
 	
-	function cargaContenido() {
+	function loadContent() {
 		// Borrar datos anteriores
 		document.getElementById('contenidos').innerHTML = "";
 		document.getElementById('estados').innerHTML = "";
@@ -27,15 +26,15 @@ String.prototype.transformaCaracteresEspeciales = function() {
 		if(window.XMLHttpRequest) {
 		peticion = new XMLHttpRequest();
 		}
-		else {
-		peticion = new ActiveXObject("Microsoft.XMLHTTP");
+			else {
+				peticion = new ActiveXObject("Microsoft.XMLHTTP");
 		}
 
 		// Preparar función de respuesta
-		peticion.onreadystatechange = muestraContenido;
+		peticion.onreadystatechange = showContent;
 
 		// Realizar petición
-		tiempoInicial = new Date();
+		initTime = new Date();
 		
 		var recurso = document.getElementById('recurso').value;
 		
@@ -43,29 +42,29 @@ String.prototype.transformaCaracteresEspeciales = function() {
 		peticion.send(null);
 	}
 	
-	function muestraContenido() {
-		var tiempoFinal = new Date();
-		var milisegundos = tiempoFinal - tiempoInicial;
+	function showContent() {
+		var finalTime = new Date();
+		var milisegundos = finalTime - initTime;
 
 		var estados = document.getElementById('estados');
-		estados.innerHTML += "[" + milisegundos + " mseg.] " + estadosPosibles[peticion.readyState] + "<br/>";
+		estados.innerHTML += "[" + milisegundos + " mseg.] " + states[peticion.readyState] + "<br/>";
 
 		if(peticion.readyState == 4) {
 			if(peticion.status == 200) {
 				var contenidos = document.getElementById('contenidos');
 				contenidos.innerHTML = peticion.responseText.transformaCaracteresEspeciales();
 			}
-			muestraCabeceras();
-			muestraCodigoEstado();
+			showHeads();
+			showStateCod();
 		}
 	}
 	
-	function muestraCabeceras() {
+	function showHeads() {
 		var cabeceras = document.getElementById('cabeceras');
 		cabeceras.innerHTML = peticion.getAllResponseHeaders().transformaCaracteresEspeciales();
 	}
 	
-	function muestraCodigoEstado() {
+	function showStateCod() {
 		var codigo = document.getElementById('codigo');
 		codigo.innerHTML = peticion.status + "<br/>" + peticion.statusText;
 	}
